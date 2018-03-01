be2d588aede3ddad44d90a98e70bd9701baa6536
Alvaro Garcia <alvaro.garcia@codurance.com>
2018-03-01 13:31:54 +0100

    Spike for the queries and the resolvers

--- src/main/kotlin/com/example/favoritesgateway/DemoApplication.kt
+++ src/main/kotlin/com/example/favoritesgateway/DemoApplication.kt
@@ -1,5 +1,7 @@
 package com.example.favoritesgateway
 
+import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import com.coxautodev.graphql.tools.GraphQLResolver
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -30,23 +33,38 @@ class FavoritesGatewayController {
         println(userId)
         println(country)
         println(language)
-//        val result = Meest.parse(rawRequest)
-//                .flatMap { meest.request(it) }
-//                .bimap({
-//                    ResponseEntity.status(it.first().code).body(map(it))
-//                }, {
-//                    ResponseEntity.ok().body(it)
-//                })
-//        if (result.isLeft()) {
-//            return result.swap().get()
-//        } else {
-//            return result.get()
-//        }
+
+//        SchemaParser().parse(Paths.get("schema.graphqls").toFile())
+//                .makeExecutableSchema()
         throw IllegalArgumentException("")
     }
 
     @ExceptionHandler(IllegalArgumentException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun illegalArgumentException() {
+    }
+}
+
+class Query : GraphQLQueryResolver {
+    private val productDao: ProductDao? = null
+    fun getUserFavorites(userId: String, country: String, language: String): List<Favorite> {
+        return productDao?.fetch(userId)!!.map { Favorite(id = "1", product = it) }
+    }
+}
+
+class ProductDao {
+    fun fetch(userId: String): List<Product> {
+        return listOf(Product("1", "google.com/image.jpg"), Product("2", "yahoo.com/image.jpg"))
+    }
+}
+
+data class Favorite(val id: String, val product: Product)
+data class Product(val id: String, val image: String)
+
+class FavoriteResolver : GraphQLResolver<Favorite> {
+    private val authorDao: AuthorDao? = null
+
+    fun getAuthor(post: Post): Author {
+        return authorDao!!.getAuthorById(post.getAuthorId())
     }
 }
